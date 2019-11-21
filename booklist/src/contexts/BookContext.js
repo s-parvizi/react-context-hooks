2282370cdf66030e21a1cefd71aae11d9302d362
s-parvizi <sohail.parvizi@gmail.com>
2019-11-21 10:34:03 +0330

    Reading List Project (part 1)

---
+++ booklist/src/contexts/BookContext.js
@@ -0,0 +1,24 @@
+import React, { createContext, useState } from "react";
+import uuid from "uuid/v1";
+
+export const BookContext = createContext();
+
+const BookContextProvider = props => {
+  const [books, setBooks] = useState([
+    { title: "name of the wind", id: 1 },
+    { title: "the final empire", id: 2 }
+  ]);
+  const addBook = (title, author) => {
+    setBooks([...books, { title, author, id: uuid() }]);
+  };
+  const removeBook = id => {
+    setBooks(books.filter(book => book.id !== id));
+  };
+  return (
+    <BookContext.Provider value={{ books, addBook, removeBook }}>
+      {props.children}
+    </BookContext.Provider>
+  );
+};
+
+export default BookContextProvider;
