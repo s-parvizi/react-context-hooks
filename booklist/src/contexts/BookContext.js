2da06380a653e97d2bfcd3626f31094c83af6825
s-parvizi <sohail.parvizi@gmail.com>
2019-11-25 08:16:02 +0330

    Reading List App (part 2)

--- booklist/src/contexts/BookContext.js
+++ booklist/src/contexts/BookContext.js
@@ -5,8 +5,8 @@ export const BookContext = createContext();
 
 const BookContextProvider = props => {
   const [books, setBooks] = useState([
-    { title: "name of the wind", id: 1 },
-    { title: "the final empire", id: 2 }
+    { title: "name of the wind", author: "patrick rothfuss", id: 1 },
+    { title: "the final empire", author: "brand sanderson", id: 2 }
   ]);
   const addBook = (title, author) => {
     setBooks([...books, { title, author, id: uuid() }]);
