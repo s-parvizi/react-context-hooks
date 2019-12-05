03b2205c2b307a2a9a3d3e3e21ef1209720c0add
s-parvizi <sohail.parvizi@gmail.com>
2019-12-05 09:49:35 +0330

    Reading List App (part 3)

--- booklist/src/App.js
+++ booklist/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import BookContextProvider from "./contexts/BookContext";
 import Navbar from "./components/Navbar";
 import Booklist from "./components/BookList";
+import NewBookForm from "./components/BookForm";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BookContextProvider>
         <Navbar />
         <Booklist />
+        <NewBookForm />
       </BookContextProvider>
     </div>
   );
